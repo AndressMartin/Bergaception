c7f460ac80102a64cdbd7f234c100aebb241cc15
Kelvin Santos <kelvin.didiei3@gmail.com>
2022-02-20 23:09:23 -0300

    Scripts de Interação
    Os scripts para um objeto ser interagível e outro para procurar e chamar a função de interação desses objetos foram adicionados ao jogo.

---
+++ Assets/Scripts/Interacao/Interacao.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interacao : MonoBehaviour
+{
+    private BoxCollider boxCollider;
+    [SerializeField] private LayerMask m_LayerMask;
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    public void Interagir()
+    {
+        Collider[] hitColliders = Physics.OverlapBox(boxCollider.bounds.center, boxCollider.bounds.extents, Quaternion.identity, m_LayerMask);
+
+        foreach(Collider objeto in hitColliders)
+        {
+            ObjetoInteragivel objetoInteragivel = objeto.GetComponent<ObjetoInteragivel>();
+
+            if(objetoInteragivel != null)
+            {
+                if(objetoInteragivel.Ativo == false)
+                {
+                    continue;
+                }
+
+                objetoInteragivel.Interagir();
+                break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Interacao/ObjetoInteragivel.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ObjetoInteragivel : MonoBehaviour
+{
+    private bool ativo;
+    private UnityEvent eventos;
+
+    public bool Ativo => ativo;
+
+    private void Start()
+    {
+        ativo = true;
+    }
+
+    public void SetAtivo(bool ativo)
+    {
+        this.ativo = ativo;
+    }
+
+    public void Interagir()
+    {
+        eventos?.Invoke();
+    }
+}
