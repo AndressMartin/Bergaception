babc63e8f303175d172d347a256391b5a6817f14
Andress Martin <42077892+AndressMartin@users.noreply.github.com>
2022-02-23 14:19:23 -0300

    Update CharacterSO.cs

--- Assets/Scripts/ScriptableObjects/CharacterSO.cs
+++ Assets/Scripts/ScriptableObjects/CharacterSO.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Characters")]
 public class CharacterSO : ScriptableObject
 {
-    public int vida;
     public int vidaMax;
     public int dano;
     public int velocidade;
