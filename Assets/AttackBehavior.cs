4b314f36315e8520891a771b5767116ba7dcb388
Andress Martin <42077892+AndressMartin@users.noreply.github.com>
2022-02-26 09:56:42 -0300

    Cinematic, organization and merge scene

--- Assets/AttackBehavior.cs
+++ Assets/AttackBehavior.cs
@@ -7,7 +7,6 @@ public class AttackBehavior : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Debug.Log("WORKING");
         animator.SetBool("Attacking", false);
     }
 
