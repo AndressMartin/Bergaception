8df0c3b988c42751ff87746235cb63d169e818cd
Andress Martin <42077892+AndressMartin@users.noreply.github.com>
2022-02-27 00:17:11 -0300

    Shaders, illumination, fixes, portal

--- Assets/InteracaoPes.cs
+++ Assets/InteracaoPes.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class InteracaoPes : MonoBehaviour
 {
-
+    public VoidEvent cruzouPortal;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Water"))
         {
             Debug.Log("Water");
             GetComponentInParent<StarterAssets.ThirdPersonController>().LaunchUpwards();
+        }
+        if (other.CompareTag("Portal"))
+        {
+            Debug.Log("Portal");
+            cruzouPortal.Raise();
         }
     }
 }
